fbd0325ea9b6dc654eb2f275dd83c63115ea1929
Kentaro Okushiro <ken09460308@gmail.com>
2018-12-16 22:59:45 +0900

    初稿

---
+++ js/script.js
@@ -0,0 +1,83 @@
+
+var newPostRef = firebase.database().ref();
+
+$('#new').click(function() {
+    make();
+});
+
+$('#del').click(function() {
+    out();
+    save();
+});
+
+$('#reset').click(function() {
+    newPostRef.remove();
+    location.reload(true);
+});
+
+function make() {
+    var sticky = $('<div class="sticky"></div>');
+    sticky.appendTo('body')
+    .css('background-color', $('#color').val())
+    .draggable({stop: saveout})
+    .dblclick(function() {
+        $(this).html('<textarea>' + $(this).html() + '</textarea>')
+        .children()
+        .focus()
+        .blur(function() {
+            $(this).parent().html($(this).val());
+            save();
+        });
+    }).mousedown(function() {
+        $('.sticky').removeClass('selected');
+        $(this).addClass('selected');
+    });
+    return sticky;
+}
+
+function out(){
+    $('.selected').remove();
+}
+
+function saveout(){
+    var items = [];
+    $('.sticky').each(function() {
+      items.push({
+        css: {
+          left: $(this).css('left'),
+          top: $(this).css('top'),
+          backgroundColor: $(this).css('background-color')
+        },
+        html: $(this).html()
+      });
+    });
+    newPostRef.set(JSON.stringify(items));
+    $('.selected').remove();
+}
+
+function save(){
+    var items = [];
+    $('.sticky').each(function() {
+      items.push({
+        css: {
+          left: $(this).css('left'),
+          top: $(this).css('top'),
+          backgroundColor: $(this).css('background-color')
+        },
+        html: $(this).html()
+      });
+    });
+    newPostRef.set(JSON.stringify(items));
+}
+    
+newPostRef.on('value', function(data){
+    let v = data.val();
+    let k = data.key;
+    if(v==null){return};
+    var items = JSON.parse(v);
+    $('.sticky').remove();
+    $.each(items, function(i, item) {
+        make().css(item.css).html(item.html);
+    });
+});
+
